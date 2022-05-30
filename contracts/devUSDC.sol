d2641524513d7bd226b15a41e26a880a6c730337
Fazeel Ahmed <58031420+thefazeelahmed@users.noreply.github.com>
2022-05-30 15:29:26 +0500

    Token Deployed

--- contracts/devUSDC.sol
+++ contracts/devUSDC.sol
@@ -3,8 +3,9 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract USDCToken is ERC20 {
-    constructor() ERC20("USD", "devUSDC") {
-        _mint(msg.sender, 200000000000000000000000000000000);
+ontract devUSDC is ERC20 {
+    constructor() ERC20("devUSDC", "DUSDC") {
+        _mint(msg.sender,200000000000000000000000);
     }
 }
+
