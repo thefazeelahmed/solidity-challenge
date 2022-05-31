ec7c7875ab6ac064a540e052a226ec5e54859180
Fazeel Ahmed <58031420+thefazeelahmed@users.noreply.github.com>
2022-05-31 14:45:00 +0500

    Staking Contract After Transfers

--- contracts/devUSDC.sol
+++ contracts/devUSDC.sol
@@ -3,8 +3,8 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract devUSDC is ERC20 {
+contract SimpleToken is ERC20 {
     constructor() ERC20("devUSDC", "DUSDC") {
-        _mint(msg.sender, 200000000000000000000000);
+        _mint(_msgSender(), 10000 * (10**uint256(decimals())));
     }
 }
